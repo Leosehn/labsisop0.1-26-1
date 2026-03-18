4e147cbebfa9f4c877985fd2e3249165abe6a754
chico sehn <79259806+Leosehn@users.noreply.github.com>
2026-03-18 18:14:34 +0000

    yes5

--- linuxdistro/buildroot-2025.02.11/custom-scripts/pre-build.sh
+++ linuxdistro/buildroot-2025.02.11/custom-scripts/pre-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 HOST=`ip route show | head --lines 1 | awk '{print $9}'`
-cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[IP-DO-HOST\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
+cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[10.0.10.252\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
