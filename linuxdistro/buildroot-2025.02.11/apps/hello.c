0366eddd6d99e5a285780366723d0faf3ab66bdd
chico sehn <79259806+Leosehn@users.noreply.github.com>
2026-03-18 22:01:09 +0000

    yes6

---
+++ linuxdistro/buildroot-2025.02.11/apps/hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("                  -`\n");
+    printf("                 .o+`\n");
+    printf("                `ooo/\n");
+    printf("               `+oooo:\n");
+    printf("              `+oooooo:\n");
+    printf("              -+oooooo+:\n");
+    printf("            `/:-:++oooo+:\n");
+    printf("           `/++++/+++++++:\n");
+    printf("          `/++++++++++++++:\n");
+    printf("         `/+++ooooooooooooo/`\n");
+    printf("        ./ooosssso++osssssso+`\n");
+    printf("       .oossssso-````/ossssss+`\n");
+    printf("      -osssssso.      :ssssssso.\n");
+    printf("     :osssssss/        osssso+++.\n");
+    printf("    /ossssssss/        +ssssooo/-\n");
+    printf("  `/ossssso+/:-        -:/+osssso+-\n");
+    printf(" `+sso+:-`                 `.-/+oso:\n");
+    printf("`++:.                           `-/+/\n");
+    printf(".`                                 `\n");
+
+    return 0;
+}
--- linuxdistro/buildroot-2025.02.11/custom-scripts/pre-build.sh
+++ linuxdistro/buildroot-2025.02.11/custom-scripts/pre-build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 HOST=`ip route show | head --lines 1 | awk '{print $9}'`
-cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[10.0.10.252\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
+cat $BASE_DIR/../custom-scripts/network-config | sed 's/\[IP-DO-HOST\]/'"$HOST"'/g' > $BASE_DIR/../custom-scripts/S41network-config
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+cp $BASE_DIR/../apps/hello $BASE_DIR/target/usr/bin
+cp $BASE_DIR/../custom-scripts/hello $BASE_DIR/target/etc/init.d/S50hello
+chmod +x $BASE_DIR/target/etc/init.d/S50hello
